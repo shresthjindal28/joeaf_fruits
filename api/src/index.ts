74b6626b2cf64e7a3aaf5229eb25cc1881922862
Algo53 <varunjangra4953@gmail.com>
2025-03-12 10:16:14 +0530

    Update origin URL in CORS

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 ConnectToMongoDB();
 
 app.use(cors({
-  origin: "https://furitsellingsite.vercel.app",
+  origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
